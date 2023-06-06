1cb77bde1e969e4ddf415722dc9da5c93b2b893f
v1tal4ik <dariia.zhukovska@gmail.com>
2023-06-06 17:51:25 +0300

    added list view switcher

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+import styles from "./MainPage.module.css";
+import TourList from "../TourList/TourList";
+import ListViewSwitcher from "../ListViewSwitcher/ListViewSwitcher";
+
+interface IProps {
+  isLight: boolean;
+}
+
+export default function MainPage({ isLight }: IProps) {
+  const [isListView, setListView] = useState(true);
+
+  const handleViewChange = (isList: boolean) => {
+    setListView(isList);
+  };
+
+  return (
+    <div className={`${isLight ? styles.light : styles.dark}`}>
+      <div className={styles.navContainer}>
+        <div className={`${isLight ? styles.title : styles.darkTitle}`}>
+          Creative Trails - Exclusive tours
+        </div>
+        <div className={styles.left}>
+          <div className={styles.inputContainer}>
+            <div className={styles.inputBox}>
+              <input
+                type="text"
+                placeholder="Search tour by name"
+                className={styles.searchInput}
+                // onChange={}
+                // value={}
+              />
+              <i className={styles.search}></i>
+            </div>
+          </div>
+          <div className={styles.listView}>
+            <ListViewSwitcher
+              isLight={isLight}
+              isList={isListView}
+              onViewChange={handleViewChange}
+            />
+          </div>
+        </div>
+      </div>
+
+      <TourList isLight={isLight} view={isListView} />
+    </div>
+  );
+}
--- src/components/Tour/Tour.tsx
+++ src/components/Tour/Tour.tsx
@@ -5,10 +5,11 @@ import styles from "./Tour.module.css";
 interface IProps {
   tourItemData: ITourListData;
   isLight: boolean;
+  view: boolean;
 }
 
-export default function Tour({ tourItemData, isLight }: IProps) {
-  return (
+export default function Tour({ tourItemData, isLight, view }: IProps) {
+  return view ? (
     <li className={`${isLight ? styles.tourItem : styles.darkTourItem}`}>
       <div>
         <img
@@ -34,6 +35,38 @@ export default function Tour({ tourItemData, isLight }: IProps) {
           {tourItemData.description}
         </div>
       </div>
+    </li>
+  ) : (
+    <li
+      className={`${styles.listView} ${
+        isLight ? styles.tourItem : styles.darkTourItem
+      } `}
+    >
+      <div>
+        <img
+          src={tourItemData.image}
+          alt={tourItemData.title}
+          className={styles.tourItemImageList}
+        />
+        <div className={styles.tourDescriptionList}>
+          <div className={styles.tourItemTitle}>{tourItemData.title}</div>
+          <div className={styles.tourItemPriceLine}>
+            <div className={styles.tourItemPrice}>
+              {`${tourItemData.price} $`}
+            </div>
+          </div>
+          <div className={styles.tourItemShortDescription}>
+            {tourItemData.description}
+          </div>
+        </div>
+      </div>
+
+      <button
+        className={styles.tourItemButtonList}
+        onClick={() => console.log("Button clicked")}
+      >
+        View
+      </button>
     </li>
   );
 }
